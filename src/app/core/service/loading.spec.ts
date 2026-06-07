be4c4e94602bfb0c0a61f510204518b56dfda8df
YasmineBOU <yasmineboudjemai10@gmail.com>
2026-06-07 20:01:00 +0200

    test: overall tests done for 'LoadingService'

--- src/app/core/service/loading.spec.ts
+++ src/app/core/service/loading.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { LoadingService } from './loading';
 
-import { Loading } from './loading';
-
-describe('Loading', () => {
-  let service: Loading;
+describe('LoadingService', () => {
+  let service: LoadingService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Loading);
+    service = TestBed.inject(LoadingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should emit false by default', (done) => {
+    service.isLoading$.subscribe(value => {
+      expect(value).toBe(false);
+      done();
+    });
+  });
+
+  it('should emit true after show()', (done) => {
+    service.show();
+    service.isLoading$.subscribe(value => {
+      expect(value).toBe(true);
+      done();
+    });
+  });
+
+  it('should emit false after hide()', (done) => {
+    service.show();
+    service.hide();
+    service.isLoading$.subscribe(value => {
+      expect(value).toBe(false);
+      done();
+    });
   });
 });
