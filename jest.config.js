a58104c4732f07cd4ca33475bf10dccd68f38228
YasmineBOU <yasmineboudjemai10@gmail.com>
2026-06-19 08:55:36 +0200

    chore: update Jest configuration for better test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/app/**/*.ts", // Source files
+    "!src/app/**/*.spec.ts", // Exclude test files
+  ],
+  coverageDirectory: "coverage", // Coverage output directory
+  coverageReporters: ["html"],
   testPathIgnorePatterns: ['<rootDir>/cypress/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
